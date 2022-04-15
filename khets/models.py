8724ffa11e3b0772291b9f2119f248439b4f7c42
dinavod12 <96486509+dinavod12@users.noreply.github.com>
2022-04-15 16:10:07 +0530

    Update models.py

--- khets/models.py
+++ khets/models.py
@@ -6,12 +6,12 @@ from django.urls import reverse
 class Post(models.Model):
    name=models.CharField(max_length=200)
    content=models.TextField()
-   address=models.CharField(max_length=200)
+   Phone_no=models.CharField(max_length=200)
    Date=models.DateTimeField(default=timezone.now)
    author=models.ForeignKey(User,on_delete=models.CASCADE)
 
    def __str__(self):
-       return f'{self.name},{self.address}'
+       return f'{self.name},{self.Phone_no}'
 
    def get_absolute_url(self):
        return reverse('blog-detail',kwargs={'pk':self.pk}) 
